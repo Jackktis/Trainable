c97423760149f509e898d96e0d58f10097a26e18
kristoffer <180500@student.dtu.dk>
2022-02-17 13:11:48 +0100

    Initial Commit

---
+++ Trainable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trainable
+//
+//  Created by Kristoffer Baumgarten on 17/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trainable/TrainableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainableApp.swift
+//  Trainable
+//
+//  Created by Kristoffer Baumgarten on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrainableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

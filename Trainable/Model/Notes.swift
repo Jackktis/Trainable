25b4097e555996fb85d8b3ce0afac09a0096b02d
kristoffer <180500@student.dtu.dk>
2022-04-08 00:14:26 +0200

    OberservableObject fungere

--- Trainable/Model/Notes.swift
+++ Trainable/Model/Notes.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 
 class Notes: ObservableObject{
@@ -15,15 +16,40 @@ class Notes: ObservableObject{
     
     
     
-    struct Note: Identifiable{
-        var id: String
-        var maxWeight: Int
-        var maxRep: Int
-    }
-    
     // Vi tilføjer Note ind i notes med ID af exercises 
     func addNote(note: Note){
         notes[note.id] = note
     }
     
+    
+    func getMaxWeight(exerciseId: String) -> Int{
+        var weight: Int = 0
+        if(!(notes[exerciseId] == nil)){
+            weight = notes[exerciseId]!.maxWeight
+        }
+        return weight
+    }
+    func getMaxRepetitions(exerciseId: String) -> Int{
+        var repetitions: Int = 0
+        if(!(notes[exerciseId] == nil)){
+            repetitions = notes[exerciseId]!.maxRepetition
+        }
+        return repetitions
+    }
+    
+    func setMaxWeight(exercisesId: String, maxWeight: Int){
+        let maxRep = getMaxRepetitions(exerciseId: exercisesId)
+        addNote(note: Note.init(id: exercisesId, maxWeight: maxWeight, maxRepetition: maxRep))
+    }
+    func setMaxRepetitions(exercisesId: String, maxRep: Int){
+        let maxWeight = getMaxWeight(exerciseId: exercisesId)
+        addNote(note: Note.init(id: exercisesId, maxWeight: maxWeight, maxRepetition: maxRep))
+    }
+    
+}
+
+struct Note: Identifiable{
+    var id: String
+    var maxWeight: Int
+    var maxRepetition: Int
 }
